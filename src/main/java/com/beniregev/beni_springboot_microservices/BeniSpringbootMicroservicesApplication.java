d774feb4649e7995725494d8e82c1abbd9bbd09e
binyamin.regev <beniregev@gmail.com>
2024-12-21 14:36:19 +0200

    Initial commit - creating Parent Spring Boot application

---
+++ src/main/java/com/beniregev/beni_springboot_microservices/BeniSpringbootMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.beni_springboot_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeniSpringbootMicroservicesApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(BeniSpringbootMicroservicesApplication.class, args);
+   }
+
+}
