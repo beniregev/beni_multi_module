2c624b569fff5e9f40077e2f1cc7f356544e3a8d
binyamin.regev <beniregev@gmail.com>
2024-12-21 23:18:18 +0200

    Add Annotations for Swagger springdoc-openapi in ShuffleController and LogController - WIP

--- service-log/src/main/java/com/beniregev/servicelog/controller/LogController.java
+++ service-log/src/main/java/com/beniregev/servicelog/controller/LogController.java
@@ -1,6 +1,9 @@
 package com.beniregev.servicelog.controller;
 
 import com.beniregev.servicelog.service.LogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,12 @@ public class LogController {
       this.logService = logService;
    }
 
+   @Operation(summary = "Log the request body (on the console")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "The request body was logged successfully to the console (for the demo)"),
+           @ApiResponse(responseCode = "422", description = "The request body is empty and an IllegalArgumentException is thrown or the request body is missing either 'logLevel' or 'message' properties, or both, and a NoSuchFieldException was thrown"),
+           @ApiResponse(responseCode = "500", description = "Some other exception occurred while logging the request body")
+   })
    @PostMapping("/v1/shuffledArray")
    public ResponseEntity<String> createLogMessage(@RequestBody String body) throws NoSuchFieldException {
       if (body.isEmpty())
@@ -23,11 +32,19 @@ public class LogController {
       return ResponseEntity.ok(result);
    }
 
+   @Operation(summary = "Log the request body (on the console")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "The request body was logged successfully to the console (for the demo)")
+   })
    @GetMapping(path = "/v1/ping")
    public ResponseEntity<String> ping() {
       return ResponseEntity.ok("Log-Service server is running, ready to receive and process requests.");
    }
 
+   @Operation(summary = "Ping the service-log service layer")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "service-log service layer pinged successfully - it's up and running")
+   })
    @GetMapping(path = "/v1/ping-service")
    public ResponseEntity<String> pingService() {
       return ResponseEntity.ok(logService.pingService());
--- service-shuffle/src/main/java/com/beniregev/serviceshuffle/controller/ShuffleController.java
+++ service-shuffle/src/main/java/com/beniregev/serviceshuffle/controller/ShuffleController.java
@@ -1,7 +1,10 @@
 package com.beniregev.serviceshuffle.controller;
 
 import com.beniregev.serviceshuffle.service.ShuffleService;
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/shuffle")
 @RestController
 public class ShuffleController {
-   @Value("${shuffled.controller.array.size.max")
    private static int MAX_SHUFFLED_ARRAY_SIZE = 1000;
 
    private final ShuffleService shuffleService;
@@ -18,6 +20,12 @@ public class ShuffleController {
       this.shuffleService = shuffleService;
    }
 
+   @Operation(summary = "Create a shuffled array of integers between 1 an value of size, without duplicates")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "201", description = "Shuffled array of integers created successfully"),
+           @ApiResponse(responseCode = "422", description = "either the value of size is not between 1 and 1000, or the shuffled array size is zero"),
+           @ApiResponse(responseCode = "500", description = "Some other exception occurred while logging the request body")
+   })
    @PostMapping(path="/v1/{size}")
    public ResponseEntity<Integer[]> createShuffledArray(@PathVariable final int size) {
       if ((size < 1) || (size > MAX_SHUFFLED_ARRAY_SIZE)) {
@@ -26,11 +34,19 @@ public class ShuffleController {
       return new ResponseEntity(shuffleService.generateAndLogShuffledArray(size), HttpStatus.CREATED);
    }
 
+   @Operation(summary = "Ping the service-shuffle server")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "service-shuffle server pinged successfully - it's up and running")
+   })
    @GetMapping(path = "/v1/ping")
    public ResponseEntity<String> ping() {
       return ResponseEntity.ok("Shuffle-Service server is running, ready to receive and process requests.");
    }
 
+   @Operation(summary = "Ping the service-shuffle service layer")
+   @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "service-shuffle service layer pinged successfully - it's up and running")
+   })
    @GetMapping(path = "/v1/ping-service")
    public ResponseEntity<String> pingService() {
       return ResponseEntity.ok(shuffleService.pingService());
