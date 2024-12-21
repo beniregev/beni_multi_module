bb75d8e03da801b1cf050d87597277a612abbcf9
binyamin.regev <beniregev@gmail.com>
2024-12-21 20:23:56 +0200

    Update LogServiceImpl @Service Java class - adding Javadoc and a line comment

--- service-log/src/main/java/com/beniregev/servicelog/service/impl/LogServiceImpl.java
+++ service-log/src/main/java/com/beniregev/servicelog/service/impl/LogServiceImpl.java
@@ -9,10 +9,15 @@ import org.springframework.stereotype.Service;
 public class LogServiceImpl implements LogService {
    @Override
    public String createLogMessageShuffledArray(String message) {
+      // The following code should be replaced with code to LOG the message properly
       System.out.println(message);
       return message;
    }
 
+   /**
+    * Return a String to acknowledge the code reached and received by the service.
+    * @return a String to acknowledge the code reached and received by the service
+    */
    @Override
    public String pingService() {
       return "Log service is running and ready to process requests.";
