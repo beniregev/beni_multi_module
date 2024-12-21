e3aee44cd9898763e9df559e0a1013cde1c3dfe1
binyamin.regev <beniregev@gmail.com>
2024-12-21 14:40:47 +0200

    Add Parent Spring Boot application Test Java class

---
+++ src/test/java/com/beniregev/beni_springboot_microservices/BeniSpringbootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniregev.beni_springboot_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeniSpringbootMicroservicesApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
