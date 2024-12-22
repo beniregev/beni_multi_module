573911f721e76619ad2b53c2e23474bba31e0f9b
binyamin.regev <beniregev@gmail.com>
2024-12-22 03:11:05 +0200

    Update LogServiceImpl - method createLogMessageShuffledArray(String) now returning Map<String, Object> and created a new private method to convert a JSON String to a Map<String, Object>

--- service-log/src/main/java/com/beniregev/servicelog/service/impl/LogServiceImpl.java
+++ service-log/src/main/java/com/beniregev/servicelog/service/impl/LogServiceImpl.java
@@ -1,17 +1,41 @@
 package com.beniregev.servicelog.service.impl;
 
 import com.beniregev.servicelog.service.LogService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Log4j
 @Service
 public class LogServiceImpl implements LogService {
    @Override
-   public String createLogMessageShuffledArray(String message) {
+   public Map<String, Object> createLogMessageShuffledArray(String stringJSON) {
+      ObjectMapper mapper = new ObjectMapper();
+      // Convert JSON string to Map
+      Map<String, Object> map;
+      try {
+         map = mapper.readValue(stringJSON, new TypeReference<>() {});
+      } catch (JsonProcessingException e) {
+         throw new RuntimeException(e);
+      }
+      map.put("timestamp", LocalDateTime.now());
+      StringBuffer sbMessage = new StringBuffer(map.get("timestamp").toString())
+              .append(" :: ")
+              .append(map.get("sending").toString())
+              .append(" Call -- ")
+              .append(map.get("logLevel").toString())
+              .append(": ")
+              .append(map.get("message").toString());
+
       // The following code should be replaced with code to LOG the message properly
-      System.out.println(message);
-      return message;
+      System.out.println("message content: " + map);
+      System.out.println("stringJSON: " + sbMessage.toString());
+      return map;
    }
 
    /**
